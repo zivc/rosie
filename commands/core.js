ff9933c356c727b8c83ee68db4a9f36be6c18079
Taylor, Ash <ashley.taylor@sungard.com>
2015-05-14 16:49:55 +0100

    .

--- commands/core.js
+++ commands/core.js
@@ -6,6 +6,7 @@ module.exports = function(client,config) {
 	client.addListener('message', function(from, to, message) {
 
 		var event = 'rosie:' + message.substr(0,config.bot.trigger.length) === config.bot.trigger ? message.split(' ')[1] : 'message';
+		console.log(event, {message:message, from:from, to:to});
 		eventEmitter.emit(event, {message:message, from:from, to:to});
 
 	});
