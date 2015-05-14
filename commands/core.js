c20bb06af0585b4a9589f4627700b7753d2a3fea
Taylor, Ash <ashley.taylor@sungard.com>
2015-05-14 17:19:00 +0100

    .

--- commands/core.js
+++ commands/core.js
@@ -2,7 +2,9 @@ module.exports = function(client,config,event) {
 
 	client.addListener('message', function(from, to, message) {
 
-		var eventName = 'rosie:' + (message.substr(0,config.bot.trigger.length) === config.bot.trigger ? message.split(' ')[1] : 'message');
+		if (typeof from === "undefined") console.log(arguments[3]);
+
+		var eventName = 'rosie.' + (message.substr(0,config.bot.trigger.length) === config.bot.trigger ? message.split(' ')[1] : 'message');
 		event.emit(eventName, {message:message, from:from, to:to});
 
 		console.log(eventName, {message:message, from:from, to:to});
