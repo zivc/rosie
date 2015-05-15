3d08fd36e46fb293661992bccb32f104339dc653
Taylor, Ash <ashley.taylor@sungard.com>
2015-05-15 11:57:11 +0100

    Replacing underscores to spaces

--- commands/core.js
+++ commands/core.js
@@ -2,7 +2,7 @@ module.exports = function(client,config,event) {
 
 	client.addListener('message', function(from, to, message) {
 
-		if (!from) from = arguments[3].prefix.split('!')[0];
+		if (!from) from = arguments[3].prefix.split('!')[0].replace('_', ' ');
 
 		var eventName = 'rosie:' + (message.substr(0,config.bot.trigger.length) === config.bot.trigger ? message.split(' ')[1] : 'message');
 		event.emit(eventName, {message:message, from:from, to:to});
